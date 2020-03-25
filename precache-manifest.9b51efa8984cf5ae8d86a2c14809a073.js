6bf89f5ec30d27a501848ebaa55cb06810cd88fd
Timo Neronen <tneronen@gmail.com>
2020-03-25 16:07:42 +0200

    initial commit

---
+++ precache-manifest.9b51efa8984cf5ae8d86a2c14809a073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097e8bc2946d54e9316bd2bd4c26565b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe39c7986689060360b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7fe39c7986689060360b",
+    "url": "/static/js/2.757ff72a.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.757ff72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8502d1a546f9db31db6f",
+    "url": "/static/js/main.d98aac9b.chunk.js"
+  },
+  {
+    "revision": "5a5fc8117f78183d2563",
+    "url": "/static/js/runtime-main.0d894c6d.js"
+  }
+]);
